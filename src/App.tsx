9c90042aafe00a51d45faa4e43d3864515f05ab3
Miguel Bizzi <miguel.bizzi@aglets.com.br>
2023-10-03 09:39:12 -0300

    removendo importacoes desnecessarias

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
-import { darkTheme } from "./styles/themes/darkTheme";
 
 import { GlobalStyle } from "./styles/global";
 import Home from "./pages/Home";
-import { useState } from "react";
 import Header from "./components/Header";
-
-interface ThemeState {
-	title: string;
-	theme: any;
-}
 
 function App() {
 	return (
