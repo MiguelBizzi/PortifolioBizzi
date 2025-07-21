bed5100bf172928111b1da543b99c6e34762c2ef
MiguelBizzi <migueloliveirabizzi@gmail.com>
2025-07-21 02:23:41 -0300

    feat: add new project "Marketplace Sales Manager" with details and image

--- src/hooks/use-projects.tsx
+++ src/hooks/use-projects.tsx
@@ -3,11 +3,18 @@ import { useTranslations } from 'next-intl'
 import project1Image from '@/assets/sltreinosweb.png'
 import project2Image from '@/assets/printwb.png'
 import project3Image from '@/assets/sltreinosapp.png'
+import project4Image from '@/assets/saas-marketplace.png'
 
 export const useProjects = () => {
   const t = useTranslations('ProjectsItems')
 
   return [
+    {
+      title: t('project_4.title'),
+      description: t('project_4.description'),
+      image: project4Image,
+      stack: ['React', 'TypeScript', 'Node.js', 'PostgreSQL', 'oAuth', 'REST API', 'Multi-tenant'],
+    },
     {
       title: t('project_3.title'),
       description: t('project_3.description'),
